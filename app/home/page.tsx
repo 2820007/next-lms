c8b31e0a8133db9cb16c85fac0d05b480299bf20
Ravi Mandal <2820007.cse@pietgroup.co.in>
2026-01-17 10:08:39 +0545

    add google login

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,9 +1,25 @@
 "use client"
-import {signIn} from "next-auth/react"
-const page = () => {
+import {signIn, signOut, useSession} from "next-auth/react"
+import Image from "next/image"
+const Home = () => {
+
+  const {data:session}=useSession()
+  if(session){
+    return(
+      <>
+       <Image src={session.user?.image || "mero-profile.png "} alt="profile-image" width={80} height={80}/>
+      <h1>Welcome,{session.user?.name}</h1>
+       <h1>{session.user?.email}</h1>
+       <button onClick={()=>{signOut()}}>Sign Out</button>
+      </>
+    )
+  }
   return (
-    <button onClick={()=>{signIn("google")}}>sing in with google</button>
+    <div>
+      <h1>Not logged In</h1>
+      <button onClick={()=>{signIn("google")}}>sing in with google</button>
+    </div>
   )
 }
 
-export default page
+export default Home
